e7e3b5db1273bdae1cbf603d43a3d041a23e08ba
Arman Moztarzadeh <armanmoztar@outlook.com>
2025-06-26 00:29:58 -0700

    feat: Add education section

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from .data import education
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,4 +9,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template(
+        'index.html',
+        title="MLH Fellow",
+        url=os.getenv("URL"),
+        education=education  
+    )
