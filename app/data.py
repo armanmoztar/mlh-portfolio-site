fbf53ce173909076d6072cc6d14d8d49b10107f7
Arman Moztarzadeh <armanmoztar@outlook.com>
2025-06-26 00:38:30 -0700

    feat: add work experience section

--- app/data.py
+++ app/data.py
@@ -8,3 +8,27 @@ education = [
         "description": "Pursuing a Bachelor of Science in Mathematics with a focus on computer science and data science."
     },
 ]
+
+work_experience = [
+    {
+        "role": "Software Engineer Intern",
+        "company": "Amazon",
+        "start": "May 2025",
+        "end": "Current",
+        "bullets": [
+            "Working on the Deals and Promotions team to enhance the customer experience on Amazon's platform.",
+        ],
+    },
+    {
+        "role": "Software Engineer Intern",
+        "company": "Amazon",
+        "start": "June 2024",
+        "end": "Sep 2024",
+        "bullets": [
+            "Developed a full-stack workflow for credit card multi-factor authentication (MFA) for UK retail checkout using React, enhancing payment security by 20%",
+            "Implemented a 'checkout' use case in TypeScript for the team's testing sandbox, utilizing AWS SAM and AWS Lambda to test the workflow with various inputs",
+            "Introduced two APIs to handle payment authorization, enabling seamless experimentation with minimal disruption to the existing codebase."
+        ],
+    }
+]
+
