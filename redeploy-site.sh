6b9d95374431c991077811d417cb6e3f1aa044e4
Arman Moztarzadeh <armanmoztar@outlook.com>
2025-07-05 19:45:32 +0000

    Change status messages

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,14 @@
 # Kill all existing tmux sessions
 tmux kill-server 2>/dev/null || true
 
-# 2 Navigate to project directory
+#  Navigate to project directory
 cd ~/mlh-portfolio-site || {
   echo "Project directory not found!"
   exit 1
 }
 
-# Pull  changes from GitHub main branch
-git fetch
+# Pull changes from GitHub main branch
+git pull
 git reset origin/main --hard
 
 # Activate virtual environment, install dependencies
@@ -18,5 +18,5 @@ pip install -r requirements.txt
 # Start new detached tmux session that runs Flask
 tmux new-session -d -s flask "cd ~/mlh-portfolio-site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0 --port=5000"
 
-echo "Flask app redeployed successfully!"
+echo "Flask app deployed successfully"
 
