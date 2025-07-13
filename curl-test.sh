5028992c83d5a4c3ff9814f5c8f8b8042f85e725
Arman Moztarzadeh <armanmoztar@outlook.com>
2025-07-13 15:55:25 -0700

    Add curl-test.sh to test timeline_post API

--- curl-test.sh
+++ curl-test.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 RAND=$RANDOM
 NAME="TestUser$RAND"
 EMAIL="test$RAND@example.com"
@@ -17,11 +15,11 @@ echo "$POST_RESPONSE"
 # Extract ID of created post
 POST_ID=$(echo "$POST_RESPONSE" | grep -o '"id":[0-9]*' | grep -o '[0-9]*')
 
-# 2. GET timeline posts to confirm addition
+# GET timeline posts to confirm addition
 echo -e "\nFetching all timeline posts..."
 curl -s http://127.0.0.1:5000/api/timeline_posts
 
-# 3. BONUS: DELETE the post to clean up
+# DELETE the post to clean up
 echo -e "\nDeleting the test post with ID: $POST_ID..."
 curl -s -X DELETE http://127.0.0.1:5000/api/timeline_post/$POST_ID
 
