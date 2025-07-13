8e058856b4d48c726c79feafc8d452ce1e074ed9
Arman Moztarzadeh <armanmoztar@outlook.com>
2025-07-13 16:51:35 -0700

    feat: Update redeploy-site to use systemd

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,22 +1,18 @@
-# Kill all existing tmux sessions
-tmux kill-server 2>/dev/null || true
+#!/bin/bash
 
-#  Navigate to project directory
-cd ~/mlh-portfolio-site || {
-  echo "Project directory not found!"
-  exit 1
-}
+# Navigate to project directory
+cd /root/mlh-portfolio-site
 
-# Pull changes from GitHub main branch
-git pull
-git reset origin/main --hard
+# Pull latest changes from GitHub
+git fetch && git reset origin/main --hard
 
-# Activate virtual environment, install dependencies
+# Activate virtual env
 source python3-virtualenv/bin/activate
+
+# Install Python dependencies
 pip install -r requirements.txt
 
-# Start new detached tmux session that runs Flask
-tmux new-session -d -s flask "cd ~/mlh-portfolio-site && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0 --port=5000"
+# Restart the systemd service
+sudo systemctl restart myportfolio
 
-echo "Flask app deployed successfully"
-
+echo "Site redeployed and service restarted."
